6a4afd3a46a6a2eafbb2e9011eea247eff3e464c
ProgramKingAbel <abelmorara254@gmail.com>
2023-07-10 17:59:32 +0100

    Define routes for app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Home, Details } from './Pages';
 import './styles/app.module.css';
 
 function App() {
   return (
-    <div className="App">App</div>
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path='stock' element={<Details />} />
+    </Routes>
   );
 }
 
