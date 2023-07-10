d61680249c7d5e2b0b97c66d0c079195ec8a354f
ProgramKingAbel <abelmorara254@gmail.com>
2023-07-10 18:53:51 +0100

    Confirm Render

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import stocksReducer from './features/stocks/stocksSlice';
 
-const store = configureStore({});
+const store = configureStore({
+  reducer: {
+    stocks: stocksReducer,
+  },
+});
 
 export default store;
