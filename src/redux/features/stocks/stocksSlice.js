efd5c1ee54608b7a083a8f4d8e6dfa5f27d47ebf
ProgramKingAbel <abelmorara254@gmail.com>
2023-07-11 12:49:32 +0100

    Create Slice to handle received data

--- src/redux/features/stocks/stocksSlice.js
+++ src/redux/features/stocks/stocksSlice.js
@@ -8,5 +8,18 @@ const initialState = {
 };
 
 export const fetchStocks = createAsyncThunk('stocks/fetchStocks', () => axios
-    .get('https://financialmodelingprep.com/api/v3/stock-screener?marketCapMoreThan=1000000000&betaMoreThan=1&volumeMoreThan=10000&sector=Technology&exchange=NASDAQ&dividendMoreThan=0&limit=100&apikey=a509fb191f7434273c911ac542b85dc2')
+    .get(process.env.REACT_APP_STOCKS_URL)
     .then((response) => response.data));
+
+const stocksSlice = createSlice({
+    name: stock,
+    initialState,
+    extraReducers: (builder) => {
+        builder.addCase(fetchStocks.fulfilled, (state, action) => {
+            state.stocks = action.payload;
+            state.error = '';
+        })
+    }
+})
+
+export default stocksSlice.reducer;
