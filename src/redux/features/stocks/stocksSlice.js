f9908e57475011b1f62b3d7cf445216edf009e21
ProgramKingAbel <abelmorara254@gmail.com>
2023-07-11 12:28:18 +0100

    Make async request to fetch data

--- src/redux/features/stocks/stocksSlice.js
+++ src/redux/features/stocks/stocksSlice.js
@@ -6,3 +6,7 @@ const initialState = {
     stocks: [],
     error: ''
 };
+
+export const fetchStocks = createAsyncThunk('stocks/fetchStocks', () => axios
+    .get('https://financialmodelingprep.com/api/v3/stock-screener?marketCapMoreThan=1000000000&betaMoreThan=1&volumeMoreThan=10000&sector=Technology&exchange=NASDAQ&dividendMoreThan=0&limit=100&apikey=a509fb191f7434273c911ac542b85dc2')
+    .then((response) => response.data));
